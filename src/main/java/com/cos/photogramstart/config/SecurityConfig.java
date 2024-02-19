e2305ce0e9d1fa3be9a1b74a8d19f35f8ac9cd91
projectmiluju <dnjsdyd0712@naver.com>
2024-02-20 01:42:23 +0900

    feat(시큐리티): 시큐리티 초기 설정

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 해당 파일로 시큐리티를 활성화
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+     // syoer 삭제 기존 시큐리티 기능 비활성화
+        http.authorizeRequests()
+                .antMatchers("/","/user/**","/image/**","/subscribe/**","/comment/**")
+                .authenticated()
+                .anyRequest()
+                .permitAll();
+    }
+}
