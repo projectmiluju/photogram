de93b29f4bf0d2681b18438bd3e637ce0bf52797
projectmiluju <dnjsdyd0712@naver.com>
2024-03-02 11:57:44 +0900

    feat(회원가입 구현): User 생성

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,17 +1,27 @@
 package com.cos.photogramstart.web;
 
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller // 1.IoC 2. 파일을 리턴하는 컨트롤러
 public class AuthController {
 
     private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
 
     @GetMapping("/auth/signin")
     public String signinForm(){
@@ -26,8 +36,10 @@ public class AuthController {
     @PostMapping("/auth/signup")
     public String signup(SignupDto signupDto){
 
-        log.info(signupDto.toString());
+        User user = signupDto.toEntity();
+//        log.info(signupDto.toString());
 
+        authService.회원가입(user);
         return "auth/signin";
     }
 }
