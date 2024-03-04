67148c00cf67b331a90fe1b73a07acad150c84db
projectmiluju <dnjsdyd0712@naver.com>
2024-03-04 14:06:03 +0900

    feat(회원가입): 공통응답JavaScript 생성
    회원가입 제약조건 에러시 회원가입 페이지로 리다이렉트

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package com.cos.photogramstart.handler;
 
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
 import com.cos.photogramstart.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,8 +15,8 @@ import java.util.Map;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public CMRespDto<?> validationException(CustomValidationException e){
-        return new CMRespDto<>(-1, e.getMessage(), e.getErrorMap());
+    public String validationException(CustomValidationException e){
+        return Script.back(e.getErrorMap().toString());
     }
 
 }
