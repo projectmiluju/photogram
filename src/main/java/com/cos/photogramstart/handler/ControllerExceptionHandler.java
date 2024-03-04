f379529ede65791801fbc8ad37ece35d70b84556
projectmiluju <dnjsdyd0712@naver.com>
2024-03-04 13:26:04 +0900

    feat(회원가입): 공통응답DTO 생성

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package com.cos.photogramstart.handler;
 
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,8 @@ import java.util.Map;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public Map<String, String> validationException(CustomValidationException e){
-        return e.getErrorMap();
+    public CMRespDto<?> validationException(CustomValidationException e){
+        return new CMRespDto<>(-1, e.getMessage(), e.getErrorMap());
     }
+
 }
