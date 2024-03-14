99b694c8c22165028983ccafddf6088c5c8a8b05
projectmiluju <dnjsdyd0712@naver.com>
2024-03-14 18:16:59 +0900

    feat(회원정보 수정): 회원정보 수정 로직 완료

---
+++ src/main/java/com/cos/photogramstart/service/UserService.java
@@ -0,0 +1,39 @@
+package com.cos.photogramstart.service;
+
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Transactional
+    public User 회원수정(Integer id, User user){
+
+        User userEntity = userRepository.findById(id).get();
+
+
+        userEntity.setName(user.getName());
+
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+
+        userEntity.setPassword(encPassword);
+        userEntity.setBio(user.getBio());
+        userEntity.setWebsite(user.getWebsite());
+        userEntity.setPhone(user.getPhone());
+        userEntity.setGender(user.getGender());
+
+
+        return userEntity;
+    }
+
+}
--- src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -1,18 +1,29 @@
 package com.cos.photogramstart.web.api;
 
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.service.UserService;
+import com.cos.photogramstart.web.dto.CMRespDto;
 import com.cos.photogramstart.web.dto.user.UserUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserApiController {
 
+    private final UserService userService;
+
     @PutMapping("/api/user/{id}")
-    public String update(UserUpdateDto userUpdateDto){
+    public CMRespDto<?> update(@PathVariable Integer id, UserUpdateDto userUpdateDto,
+                               @AuthenticationPrincipal PrincipalDetails principalDetails){
 
-        System.out.println(userUpdateDto);
-        return "update 메서도 실행";
-
+        User userEntity = userService.회원수정(id, userUpdateDto.toEntity());
+        principalDetails.setUser(userEntity);
+        return new CMRespDto<>(1, "회원정보수정완료", userEntity);
     }
 }
