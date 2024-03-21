7916136516308cd4792886d8acc387783e2c8708
projectmiluju <dnjsdyd0712@naver.com>
2024-03-21 20:06:19 +0900

    feat(구독정보 view): 구독정보 리스트 퀴리 작성

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -2,9 +2,15 @@ package com.cos.photogramstart.service;
 
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.web.dto.subscribe.SubscribeDto;
 import lombok.RequiredArgsConstructor;
+import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -12,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class SubscribeService {
 
     private final SubscribeRepository subscribeRepository;
+    private final EntityManager entityManager;
 
     @Transactional
     public void 구독하기(Integer fromUserId, Integer toUserId) {
@@ -30,5 +37,36 @@ public class SubscribeService {
 
         subscribeRepository.mUnSubscribe(fromUserId, toUserId);
 
+    }
+
+    @Transactional(readOnly = true)
+    public List<SubscribeDto> 구독리스트(Integer principalId, Integer pageUserId) {
+
+//        StringBuffer sb = new StringBuffer();
+//        sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
+//        sb.append("if((SELECT 1 FROM subscribe");
+//        sb.append("WHERE fromUserId = 1 AND toUserId = u.id),1,0) subscribeState, ");
+//        sb.append("if((1=u.id),1,0) equalUserState ");
+//        sb.append("FROM user u INNER JOIN subscribe s ");
+//        sb.append("ON u.id = s.toUserId ");
+//        sb.append("WHERE s.fromUserId = ?");
+        StringBuffer sb = new StringBuffer();
+        sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
+        sb.append("if((SELECT 1 FROM subscribe ");
+        sb.append("WHERE fromUserId = ? AND toUserId = u.id),1,0) subscribeState, ");
+        sb.append("if((?=u.id),1,0) equalUserState ");
+        sb.append("FROM user u INNER JOIN subscribe s ");
+        sb.append("ON u.id = s.toUserId ");
+        sb.append("WHERE s.fromUserId = ?");
+
+        Query query = entityManager.createNativeQuery(sb.toString())
+                .setParameter(1, principalId)
+                .setParameter(2, principalId)
+                .setParameter(3, pageUserId);
+
+        JpaResultMapper result = new JpaResultMapper();
+        List<SubscribeDto> subscribeDto = result.list(query, SubscribeDto.class);
+
+        return subscribeDto;
     }
 }
--- src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -4,19 +4,25 @@ package com.cos.photogramstart.web.api;
 import com.cos.photogramstart.config.auth.PrincipalDetails;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
+import com.cos.photogramstart.service.SubscribeService;
 import com.cos.photogramstart.service.UserService;
 import com.cos.photogramstart.web.dto.CMRespDto;
+import com.cos.photogramstart.web.dto.subscribe.SubscribeDto;
 import com.cos.photogramstart.web.dto.user.UserUpdateDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -24,6 +30,7 @@ import java.util.Map;
 public class UserApiController {
 
     private final UserService userService;
+    private final SubscribeService subscribeService;
 
     @PutMapping("/api/user/{id}")
     public CMRespDto<?> update(
@@ -44,5 +51,15 @@ public class UserApiController {
             principalDetails.setUser(userEntity);
             return new CMRespDto<>(1, "회원수정완료", userEntity);
         }
+    }
+
+    @GetMapping("/api/user/{pageUserId}/subscribe")
+    public ResponseEntity<?> subscribeList(@PathVariable Integer pageUserId,
+                                           @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        List<SubscribeDto> subscribeDto = subscribeService.구독리스트(principalDetails.getUser().getId(), pageUserId);
+
+        return new ResponseEntity<>(
+                new CMRespDto<>(1, "구독정보 리스트 가져오기 성공", subscribeDto), HttpStatus.OK);
     }
 }
